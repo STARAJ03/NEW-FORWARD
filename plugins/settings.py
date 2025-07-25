21a96f21b627292699605a627adfc8536fdc9372
STARAJ03 <127228123+STARAJ03@users.noreply.github.com>
2025-07-25 14:17:28 +0530

    Update settings.py

--- plugins/settings.py
+++ plugins/settings.py
@@ -1,7 +1,11 @@
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
 # Ask Doubt on telegram @KingVJ01
-
+from telethon import events, Button
+import re
+import os
+import string
+import random
 import asyncio 
 from database import Db, db
 from script import Script
